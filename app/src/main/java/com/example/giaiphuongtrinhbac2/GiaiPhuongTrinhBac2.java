f93012be8bf598a185ba0a7951c62558cb0aaace
FourN7 <nguyenngonhutnam@gmail.com>
2019-09-03 19:33:43 +0700

    hfhfsdfsa

--- app/src/main/java/com/example/giaiphuongtrinhbac2/GiaiPhuongTrinhBac2.java
+++ app/src/main/java/com/example/giaiphuongtrinhbac2/GiaiPhuongTrinhBac2.java
@@ -1,8 +1,51 @@
 package com.example.giaiphuongtrinhbac2;
 
 public class GiaiPhuongTrinhBac2 {
-    double detal, x1, x2
-  public double GiaiPhuongTrinhBac2{
+    float a;
+    float b;
+    float c;
+    float x1;
+    float x2;
+    float x;
 
+    public float getX() {
+        return x;
+    }
+
+    public float getX1() {
+        return x1;
+    }
+
+    public float getX2() {
+        return x2;
+    }
+
+    public float GiaiPhuongTrinh(float a ,float b ,float c){
+        if (a==0){
+            if (b==0) {
+                if (c == 0) {
+                    return 1;
+                }else {
+                    return -1;
+                }
+            }else{
+                x=(-c/b);
+                return 2;
+            }
+        }else {
+            float delta = b * b - 4 *( a * c);
+            if (delta > 0) {
+                x1 = (float) (-b + Math.sqrt(delta)) / (2 * a);
+                x2 = (float) (-b - Math.sqrt(delta)) / (2 * a);
+                return 3;
+            }
+            if (delta == 0) {
+                x1 = (-b / (2 * a));
+                return 4;
+            }
+            if(delta<0)
+                return -1;
+        }
+        return 3000;
     }
 }
--- app/src/main/java/com/example/giaiphuongtrinhbac2/MainActivity.java
+++ app/src/main/java/com/example/giaiphuongtrinhbac2/MainActivity.java
@@ -10,22 +10,38 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView Ketqua;
-    EditText a ,b ,c ;
+    EditText nhapa ,nhapb ,nhapc ;
     Button tinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        a=findViewById(R.id.editText_nhapa);
-        b=findViewById(R.id.editText_nhapb);
-        c=findViewById(R.id.editText_nhapc);
+        nhapa=findViewById(R.id.editText_nhapa);
+        nhapb=findViewById(R.id.editText_nhapb);
+        nhapc=findViewById(R.id.editText_nhapc);
         Ketqua=findViewById(R.id.textView_ketqua);
         tinh=findViewById(R.id.button_tinh);
         tinh.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                float a=Float.parseFloat(nhapa.getText().toString());
+                float b=Float.parseFloat(nhapb.getText().toString());
+                float c=Float.parseFloat(nhapc.getText().toString());
 
+                GiaiPhuongTrinhBac2 tinh=new GiaiPhuongTrinhBac2();
+
+                if (tinh.GiaiPhuongTrinh(a,b,c)==-1)
+                    Ketqua.setText("Phuong trinh vo nghiem");
+                if (tinh.GiaiPhuongTrinh(a,b,c)==3)
+                    Ketqua.setText("Phuong trinh 2 nghiem:"+tinh.getX1()+"va"+tinh.getX2());
+                if(tinh.GiaiPhuongTrinh(a,b,c)==4)
+                    Ketqua.setText("Phuong co nghiem kep"+tinh.getX1());
+                if (tinh.GiaiPhuongTrinh(a,c,b)==2);
+                    Ketqua.setText("Phuong Trinh co 1 nghiem:"+tinh.getX());
+                if(tinh.GiaiPhuongTrinh(a,c,b)==1)
+                    Ketqua.setText("Phuong trinh vo so nghiem");
             }
         });
 
