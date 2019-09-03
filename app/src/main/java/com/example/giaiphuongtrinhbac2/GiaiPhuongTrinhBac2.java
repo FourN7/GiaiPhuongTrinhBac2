adcd84a354e2e830e0989dda76d0e641755d174b
FourN7 <nguyenngonhutnam@gmail.com>
2019-09-03 17:12:00 +0700

    hfh

---
+++ app/src/main/java/com/example/giaiphuongtrinhbac2/GiaiPhuongTrinhBac2.java
@@ -0,0 +1,8 @@
+package com.example.giaiphuongtrinhbac2;
+
+public class GiaiPhuongTrinhBac2 {
+    double detal, x1, x2
+  public double GiaiPhuongTrinhBac2{
+
+    }
+}
--- app/src/main/java/com/example/giaiphuongtrinhbac2/MainActivity.java
+++ app/src/main/java/com/example/giaiphuongtrinhbac2/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.giaiphuongtrinhbac2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView Ketqua;
+    EditText a ,b ,c ;
+    Button tinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a=findViewById(R.id.editText_nhapa);
+        b=findViewById(R.id.editText_nhapb);
+        c=findViewById(R.id.editText_nhapc);
+        Ketqua=findViewById(R.id.textView_ketqua);
+        tinh=findViewById(R.id.button_tinh);
+        tinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
